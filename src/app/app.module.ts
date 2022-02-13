c05a0f66c2335fab2e040c2240b8d331a4950e8d
Enormous Bug <mort.mb@gmail.com>
2022-02-14 00:20:01 +0100

    component placeholders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { InfoComponent } from './components/info/info.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TimelineComponent,
+    InfoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
