0d6b3bee7a8a80e6059681ee63c546b6e504b009
Enormous Bug <mort.mb@gmail.com>
2022-02-14 22:31:00 +0100

    timeline wip

---
+++ src/app/components/timeline/element/element.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-element',
+  templateUrl: './element.component.html',
+  styleUrls: ['./element.component.scss']
+})
+export class ElementComponent implements OnInit {
+  @Input() title = '';
+  @Input() time = '';
+  @Input() icon = '';
+  @Input() mapLink='';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
