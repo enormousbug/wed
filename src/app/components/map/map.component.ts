baa913c50558a8e938e48fce7a14010558d69036
Enormous Bug <mort.mb@gmail.com>
2022-02-17 00:42:46 +0100

    upodated map with new icons

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -6,6 +6,8 @@ import Vector from 'ol/source/Vector';
 import * as ol from 'ol/proj';
 import VectorLayer from 'ol/layer/Vector';
 import Point from 'ol/geom/Point';
+import { Circle, Fill, Icon, Stroke, Style } from 'ol/style';
+import { scale } from 'ol/size';
 
 @Component({
   selector: 'app-map',
@@ -28,18 +30,37 @@ export class MapComponent implements OnInit {
         center: ol.fromLonLat([17.00726, 51.04564]),
         zoom: 13,
       }),
+      controls: [],
     });
+
+    let church = new Feature({
+      geometry: new Point(ol.fromLonLat([17.02328, 51.05707])),
+    });
+    let party = new Feature({
+      geometry: new Point(ol.fromLonLat([16.99124, 51.03421])),
+    });
+    church.setStyle(
+      new Style({
+        image: new Icon({
+          crossOrigin: 'anonymous',
+          src: 'assets/church.png',
+          scale: 0.3,
+        }),
+      })
+    );
+    party.setStyle(
+      new Style({
+        image: new Icon({
+          crossOrigin: 'anonymous',
+          src: 'assets/party.png',
+          scale: 0.3,
+        }),
+      })
+    );
 
     let markers = new VectorLayer({
       source: new Vector({
-        features: [
-          new Feature({
-            geometry: new Point(ol.fromLonLat([17.02328, 51.05707])),
-          }),
-          new Feature({
-            geometry: new Point(ol.fromLonLat([16.99124, 51.03421])),
-          })
-        ],
+        features: [church, party],
       }),
     });
     this.map.addLayer(markers);
