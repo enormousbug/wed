bc69c20b7de6e7ef46851df1f50d5a9426e7b05f
Enormous Bug <mort.mb@gmail.com>
2022-02-16 01:53:07 +0100

    added mailer wip

--- src/app/components/rsvp/rsvp.component.ts
+++ src/app/components/rsvp/rsvp.component.ts
@@ -5,10 +5,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rsvp.component.scss']
 })
 export class RsvpComponent implements OnInit {
-
+  //nodemailer = require("nodemailer");
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    // Email.send({
+    //   Host: 'smtp.elasticemail.com',
+    //   Username: 'new.guest@weselekarolinyimichala.fun',
+    //   Password: '5ECF2ECC68A800A7C487C505F0A9EE3EB84C',
+    //   To: 'we.wesele1@gmail.com',
+    //   From: 'new.guest@weselekarolinyimichala.fun',
+    //   Subject: 'GuestConfirmation',
+    //   Body: ''
+    // });
+  }
 }
